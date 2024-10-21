eb5523faae90ec007f7ab7c1a2ec10de96de5b82
bryanvullo <bryanvullo04@gmail.com>
2024-10-21 18:40:30 +0100

    register player azure function

--- function_app.py
+++ function_app.py
@@ -3,6 +3,10 @@ import logging
 
 app = func.FunctionApp()
 
+# TODO - add uniqueness in player username
+# TODO - player "id" partition key
+# TODO - prompt "username" partition key
+
 @app.route(route="myFirstFunction", auth_level=func.AuthLevel.ANONYMOUS)
 def myFirstFunction(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -24,3 +28,27 @@ def myFirstFunction(req: func.HttpRequest) -> func.HttpResponse:
              status_code=200
         )
     
+@app.route(route="player/register", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
+def registerPlayer(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request. Register Player')
+
+    input = req.get_json()
+    username = input.get('username')
+    password = input.get('password')
+
+    if len(username) < 5 or len(password) > 15:
+        return func.HttpResponse(
+            body = json.dumps({"result": False, "msg": "Username less than 8 characters or more than 15 characters" })
+        )
+    if len(password) < 8 or len(password) > 15:
+        return func.HttpResponse(
+            body = json.dumps({"result": False, "msg": "Password less than 8 characters or more than 15 characters" })
+        )
+    
+    # TODO: check if username already exists
+
+    # else
+    # TODO create player in DB, set GP ans TS to 0
+    return func.HttpResponse(
+        body = json.dumps({"result": True, "msg": "OK" })
+    )
