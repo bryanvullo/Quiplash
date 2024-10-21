95c9eb119abe80fc46769ea1fe11e9fd52f58610
bryanvullo <bryanvullo04@gmail.com>
2024-10-21 11:10:23 +0100

    created my first azure function. Azure init

---
+++ function_app.py
@@ -0,0 +1,25 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="myFirstFunction")
+def myFirstFunction(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "Please enter a name",
+             status_code=200
+        )
